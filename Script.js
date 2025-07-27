3976985e389b188620c54d42140b8f03b3ded394
dill-e27 <dylan92008@gmail.com>
2025-07-27 17:35:54 -0230

    Update Script.js

--- Script.js
+++ Script.js
@@ -48,7 +48,7 @@ function getWeather() {
         return;
     }
 
-    fetch(`https://weather-app-backend-z5cf.onrender.com/weather?city=${encodeURIComponent(city)}`)
+    fetch(`https://weather-app-backend-g5xy.onrender.com/weather?city=${encodeURIComponent(city)}`)
         .then(res => res.json())
         .then(data => {
             displayWeather(data.current);
